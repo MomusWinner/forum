313c3c0dea08705a572d20944ec194ed7e9450ee
MomusWinner <areg06.06@mail.ru>
2024-04-30 11:53:59 +0500

    load dotenv

--- create_schema.py
+++ create_schema.py
@@ -2,7 +2,14 @@ from psycopg2 import connect
 from dotenv import load_dotenv
 from os import getenv
 
-conn = connect(dbname=getenv('PG_DBNAME'), host=getenv('PG_HOST'), password=getenv('PG_PASSWORD'), port=getenv('PG_PORT'), user=getenv('PG_USER'))
+load_dotenv()
+
+conn = connect(dbname=getenv('PG_DBNAME'),
+               host=getenv('PG_HOST'),
+               password=getenv('PG_PASSWORD'),
+               port=getenv('PG_PORT'),
+               user=getenv('PG_USER'))
+
 cursor = conn.cursor()
 cursor.execute('create schema forum')
 conn.commit()
