30237734664312c37f736f675793f1e134aa7a81
Areg <areg06.06@mail.ru>
2024-05-21 19:06:04 +0300

    update config

--- reactapp/src/api-path.js
+++ reactapp/src/api-path.js
@@ -1,4 +1,6 @@
 
 
-export const endpoint = "http://127.0.0.1:8000/"
-export const API_LOGIN = endpoint + "api/v1/token/login"
+export const HOST = "http://127.0.0.1:8000/"
+export const API = "api/v1/"
+export const API_LOGIN = API + "token/login"
+export const SECTION = API + "section/"
