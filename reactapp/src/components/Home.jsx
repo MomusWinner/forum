f2fdac5b133a726e9c85d09147c1e1b87fc45d37
Areg <areg06.06@mail.ru>
2024-05-21 19:06:49 +0300

    add sections to home page

--- reactapp/src/components/Home.jsx
+++ reactapp/src/components/Home.jsx
@@ -1,8 +1,32 @@
-import axios from "axios"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom";
+import { Sections } from "./Sections";
 
 export function Home()
 {
+    const [token, setToken] = useState()
+    const navigate = useNavigate();
+
+    useEffect(()=>
+    {
+        let newToken = localStorage.getItem('token')
+        console.log(typeof(newToken) + " " + newToken)
+        if(newToken === undefined)
+        {   
+            console.log('artartrar') 
+            navigate('/login')
+        }
+        else{
+            setToken(newToken)
+        }
+    }, [])
+
     return(
-        <div>Home page</div>
+        <>
+        {token?
+        <Sections token={token}/>:
+        ""
+        }
+        </>
     )
 }
