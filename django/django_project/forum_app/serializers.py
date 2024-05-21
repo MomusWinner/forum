bfbbacaf30c7198bf581607555c16c4c7e0e810a
Areg <areg06.06@mail.ru>
2024-05-21 23:13:00 +0300

    fix serializers

--- django/django_project/forum_app/serializers.py
+++ django/django_project/forum_app/serializers.py
@@ -1,26 +1,28 @@
 from .models import User, Thread, Section, Message
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import HyperlinkedModelSerializer, PrimaryKeyRelatedField
 
 
 class UserSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('id', 'name', 'email')
 
 
 class ThreadSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Thread
-        fields = '__all__'
+        fields = ('id', 'title', 'user_id')
 
 
 class SectionSerializer(HyperlinkedModelSerializer):
+    threads = PrimaryKeyRelatedField(queryset=Thread.objects.all(), many=True)
+
     class Meta:
         model = Section
-        fields = '__all__'
+        fields = ('id', 'name', 'threads',)
 
 
 class MessageSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Message
-        fields = '__all__'
+        fields = ('id', 'thread_id', 'user_id', 'message_body')
