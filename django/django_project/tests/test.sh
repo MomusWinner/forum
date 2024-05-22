3699de564c0e12ce63c9d7167283f87b6c0390cf
Areg <areg06.06@mail.ru>
2024-05-22 19:58:25 +0300

    fix test.sh set correct manage.py path and fix pylint

--- django/django_project/tests/test.sh
+++ django/django_project/tests/test.sh
@@ -4,12 +4,12 @@ export PG_PORT=5432
 export PG_USER=test
 export PG_PASSWORD=test
 export PG_DBNAME=test
-export MINIO_ACCESS_KEY_ID=user
-export MINIO_SECRET_ACCESS_KEY=password
-export MINIO_STORAGE_BUCKET_NAME=static
-export MINIO_API=http://localhost:9000
-export MINIO_CONSISTENCY_CHECK_ON_START=False
+# export MINIO_ACCESS_KEY_ID=user
+# export MINIO_SECRET_ACCESS_KEY=password
+# export MINIO_STORAGE_BUCKET_NAME=static
+# export MINIO_API=http://localhost:9000
+# export MINIO_CONSISTENCY_CHECK_ON_START=False
 
 
 
-python3 manage.py test $1
+python3 django/django_project/manage.py test $1
