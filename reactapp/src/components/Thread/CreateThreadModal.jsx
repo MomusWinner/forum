6947c867a0f8f11d5df7a4fe4c19ab795c4aa830
Areg <areg06.06@mail.ru>
2024-05-28 01:01:05 +0300

    update createThreadModal add select section logic

--- reactapp/src/components/Thread/CreateThreadModal.jsx
+++ reactapp/src/components/Thread/CreateThreadModal.jsx
@@ -1,31 +1,57 @@
 import axios from "axios";
 import { Button, ModalBody, Modal, ModalFooter, ModalHeader, Input, Label } from "reactstrap";
-import { useState } from "react";
+import Select from 'react-select' 
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { HOST, THREAD } from "../../api-path";
+import { HOST, SECTION, THREAD } from "../../api-path";
 import { getToken } from "../utils"
 
+import makeAnimated from 'react-select/animated';
+
+const animatedComponents = makeAnimated();
 
 export function CreateThreadModal({onCreate})
 {
-    const { className } = "";
+    const [modal, setModal] = useState(false)
+    const [sectionsOption, setsectionsOption] = useState()
 
-    const [modal, setModal] = useState(false);
     const [title, setTitle] = useState()
-    const navigate = useNavigate();
-
-
+    const [sections, setSections] = useState()
     const toggle = () => setModal(!modal);
+    const navigate = useNavigate();
+    
+    useEffect(()=>{
+        let token = getToken(navigate)
+        axios.get(HOST + SECTION, {headers: { "Authorization": 'Token ' + token } } )
+        .then((resp)=>{
+            var sections = []
+            for(let i=0; i<resp.data.length; i++)
+            {
+                let section = new Object()
+                section.label = resp.data[i].name
+                section.value = resp.data[i].id
+                sections.push(section)
+            }
+            setsectionsOption(sections)
+        })
+        .catch((e)=>{
+            console.log(e.data)
+        })
+    }, [])
 
     function createThread(){
         let token = getToken(navigate)
-        if(title)
-        {
-            axios.post(HOST+THREAD, {title: title},  {headers: { "Authorization": 'Token ' + token } })   
+        let sectionsId = []
+        sections.forEach((section)=>sectionsId.push(section.value))
+        axios.post(HOST+THREAD, {title: title, sections: sectionsId}, {headers: { "Authorization": 'Token ' + token } })
+        .then((resp)=>{
             onCreate()
             toggle()
-        }
-
+        })
+        .catch((e) =>
+        {
+            toggle()
+        })
     }
 
     return (
@@ -35,16 +61,26 @@ export function CreateThreadModal({onCreate})
             </Button>
             <Modal
             isOpen={modal}
-            modalTransition={{ timeout: 700 }}
-            backdropTransition={{ timeout: 1300 }}
-            toggle={toggle}
-            className={className}>
+            modalTransition={{ timeout: 300 }}
+            backdropTransition={{ timeout: 700 }}
+            toggle={toggle}>
                 <ModalHeader toggle={toggle}>Create thread</ModalHeader>
                 <ModalBody>
                     <Label for="title" sm={2}>
                         Title
                     </Label>
                     <Input id="title" onChange={(e) => setTitle(e.target.value)}></Input>
+                    <Label for="sections" sm={2}>
+                        Sections
+                    </Label>
+                    <Select
+                        id="sections"
+                        closeMenuOnSelect={false}
+                        components={animatedComponents}
+                        isMulti
+                        options={sectionsOption}
+                        onChange={setSections}
+                    />
                 </ModalBody>
                 <ModalFooter>
                     <Button color="primary" onClick={createThread}>
@@ -57,5 +93,4 @@ export function CreateThreadModal({onCreate})
             </Modal>
         </div>
       );
-    
 }
