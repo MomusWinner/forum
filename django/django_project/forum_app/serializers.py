e2f9eab2b08f9a0de6bddd83d5ea202a019147aa
Areg <areg06.06@mail.ru>
2024-05-28 00:56:06 +0300

    update serializer

--- django/django_project/forum_app/serializers.py
+++ django/django_project/forum_app/serializers.py
@@ -1,13 +1,11 @@
 from .models import User, Thread, Section, Message
-from rest_framework.serializers import HyperlinkedModelSerializer, PrimaryKeyRelatedField
+from rest_framework.serializers import HyperlinkedModelSerializer, PrimaryKeyRelatedField, ModelSerializer
 
 
 class UserSerializerDAB(HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email')
-
-
 
 
 class SectionSerializer(HyperlinkedModelSerializer):
@@ -17,15 +15,36 @@ class SectionSerializer(HyperlinkedModelSerializer):
         model = Section
         fields = ('id', 'name', 'threads')
 
+class MessageSerializer(ModelSerializer):
+    def create(self, validated_data):
+        user = self.context['request'].user
+        print(user)
+        message = Message.objects.create(
+            user=user,
+            **validated_data
+        )
 
-class MessageSerializer(HyperlinkedModelSerializer):
+        return message
+
     class Meta:
         model = Message
-        fields = ('id', 'thread_id', 'user_id', 'message_body', 'created')
+        fields = ('id', 'thread', 'user', 'message_body', 'created')
 
-class ThreadSerializer(HyperlinkedModelSerializer):
-    messages = MessageSerializer(read_only =True,many=True, source='message_set')
+
+class ThreadSerializer(ModelSerializer):
+    messages = MessageSerializer(read_only=True, many=True, source='message_set', )
+    sections = PrimaryKeyRelatedField(queryset=Section.objects.all(), many=True)
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        data = {key: value for key, value in validated_data.items() if key != 'sections'}
+        thread = Thread.objects.create(
+            user=user,
+            **data
+        )
+        thread.sections.set([section.id for section in validated_data['sections']])
+        return thread
 
     class Meta:
         model = Thread
-        fields = ('id', 'title', 'user_id','created', 'messages')
+        fields = ('id', 'title', 'user_id', 'sections', 'created', 'messages')
